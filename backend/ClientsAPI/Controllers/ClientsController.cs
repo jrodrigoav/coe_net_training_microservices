173b3c4910618961f82e644feda02ee8a380ac67
Gustavo Meléndez <gus_1325@hotmail.com>
2023-11-15 16:25:03 -0600

    ClientsAPI migrated to net 8

--- backend/ClientsAPI/Controllers/ClientsController.cs
+++ backend/ClientsAPI/Controllers/ClientsController.cs
@@ -8,9 +8,9 @@ namespace ClientsAPI.Controllers
     [ApiController]
     public class ClientsController : ControllerBase
     {
-        private readonly ClientService _clientService;
+        private readonly IClientService _clientService;
 
-        public ClientsController(ClientService clientService)
+        public ClientsController(IClientService clientService)
         {
             _clientService= clientService;
         }
@@ -22,6 +22,51 @@ namespace ClientsAPI.Controllers
             var clients = await _clientService.ListAsync();
             //AWSXRayRecorder.Instance.EndSegment();
             return clients;
+        }
+
+        //[Route("/get/{id:length(38)}")]
+        [HttpGet("/get/{id:length(38)}", Name = "GetClient")]
+        //[HttpGet(Name = "GetClient")]
+        public async Task<ActionResult<Client>> Get(string id)
+        {
+            //AWSXRayRecorder.Instance.BeginSegment("Clients API");
+            var client = await _clientService.GetAsync(id);
+            //AWSXRayRecorder.Instance.EndSegment();
+
+            if (client == null)
+                return NotFound();
+
+            return client;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Client>> Create(Client client)
+        {
+            //AWSXRayRecorder.Instance.BeginSegment("Clients API");
+            await _clientService.CreateAsync(client);
+            //AWSXRayRecorder.Instance.EndSegment();
+            return Ok(client);
+        }
+
+        //[Route("/update/{id:length(37)}")]
+        [HttpPut("/update/{id:length(37)}")]
+        //[HttpPut]
+        public async Task<ActionResult<Client>> Update(string id, Client newClientData)
+        {
+            // AWSXRayRecorder.Instance.BeginSegment("Clients API");
+            var client = _clientService.GetAsync(id);
+            // AWSXRayRecorder.Instance.EndSegment();
+
+            newClientData.Id = id;
+
+            if (client == null)
+                return NotFound();
+
+            //AWSXRayRecorder.Instance.BeginSegment("Clients API");
+            await _clientService.UpdateAsync(newClientData);
+            //AWSXRayRecorder.Instance.EndSegment();
+
+            return Ok(newClientData);
         }
     }
 }
--- backend/ClientsAPI/Services/ClientService.cs
+++ backend/ClientsAPI/Services/ClientService.cs
@@ -2,11 +2,20 @@
 
 namespace ClientsAPI.Services
 {
-    public class ClientService
+    public class ClientService : IClientService
     {
+        public async Task<Client> CreateAsync(Client client)
+        {
+            client.Id = Guid.NewGuid().ToString();
+            return await Task.FromResult(new Client());
+        }
         public async Task<List<Client>> ListAsync()
         {
             return await Task.FromResult(new List<Client>());
         }
+
+        public async Task<Client> GetAsync(string id) => await Task.FromResult(new Client());
+
+        public async Task UpdateAsync(Client newClientData) => await Task.FromResult(new Client());
     }
 }
