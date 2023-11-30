ff5c659e3449519b718eaad030aceb3245f0ce39
raul.reyes <raul.reyes@unosquare.com>
2023-11-30 13:27:03 -0600

    Migration Resources API -- Added configuration on Program class -- Added Service, Controller, Interfaces, List, Get, Create, Update, Delete  methods. -- Added Models -- Documentation

---
+++ backend/ResourcesAPI/Services/ResourcesService.cs
@@ -0,0 +1,72 @@
+﻿using ResourcesAPI.Models;
+using ResourcesAPI.Services.Interface;
+
+namespace ResourcesAPI.Services
+{
+    public class ResourcesService : IResourcesService
+    {
+        public Task<ResourceModel> CreateAsync(ResourceModel Model)
+        {
+            try
+            {
+                var result = new ResourceModel();
+                return Task.FromResult(result);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public void DeleteAsync(string id)
+        {
+            try
+            {
+                var removed = id;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public Task<ResourceModel> GetAsync(string id)
+        {
+            try
+            {
+                var result = new ResourceModel();
+                return Task.FromResult(result);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public Task<List<ResourceModel>> ListAsync()
+        {
+            try
+            {
+                var list = new List<ResourceModel>();
+                return Task.FromResult(list);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public Task<ResourceModel> UpdateAsync(ResourceModel model)
+        {
+            try
+            {
+                var result = new ResourceModel();
+                return Task.FromResult(result);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
