d7772d5bf1961ebf300b0104926225719719839b
Emilio Galvan <egalvan@foundationmedicine.com>
2024-05-27 10:51:04 -0600

    Fixing update of client

--- backend/ClientsAPI/Models/UpdateClientRequest.cs
+++ backend/ClientsAPI/Models/UpdateClientRequest.cs
@@ -4,7 +4,11 @@ namespace ClientsAPI.Models
 {
     public class UpdateClientRequest : ClientRequest
     {
-        [Required, StringLength(25)]
+        //[Required, StringLength(36)]
+        //public String Id { get; init; }
+
+
+        [Required]
         public Guid Id { get; init; }
     }
 }
