2b5a73671cfec123ce6ad0c09c7f8f2327d42cde
WAlfaroP <walfaropuerta@gmail.com>
2024-06-04 21:18:49 -0500

    Create AdminLayout component

--- frontend/bookstore-react/src/components/AdminLayout.tsx
+++ frontend/bookstore-react/src/components/AdminLayout.tsx
@@ -1,8 +1,19 @@
-const Layout = () => {
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+import { Outlet } from 'react-router-dom';
+
+const AdminLayout = () => {
   return (
-    <div>
+    <div className="main-wrapper">
+      <Sidebar />
+      <div>
+        <Navbar />
+        <div className="router-content">
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default Layout;
+export default AdminLayout;
