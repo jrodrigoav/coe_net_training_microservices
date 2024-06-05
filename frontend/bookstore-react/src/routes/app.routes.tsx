e3f7917cea801b2f96cef9ba4f848728a9abd307
WAlfaroP <walfaropuerta@gmail.com>
2024-06-05 16:58:34 -0500

    Update Default route

--- frontend/bookstore-react/src/routes/app.routes.tsx
+++ frontend/bookstore-react/src/routes/app.routes.tsx
@@ -3,7 +3,7 @@ import AdminLayoutRoutes from './admin-layout.routes';
 
 const AppRouter = () => (
     <Routes>
-        <Route path="/" element={<Navigate to="clients" />} />
+        <Route path="/" element={<Navigate to="resources" />} />
         <Route path="/*" element={<AdminLayoutRoutes />} />
     </Routes>
 );
