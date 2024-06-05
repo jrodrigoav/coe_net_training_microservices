92769e5a41a4b7c8efea42edd6c3ebbbe0de1eb2
WAlfaroP <walfaropuerta@gmail.com>
2024-06-04 21:15:13 -0500

    Add route items file

---
+++ frontend/bookstore-react/src/routes/routeItems.tsx
@@ -0,0 +1,5 @@
+export const routeItems = [
+    { path: '/resources', title: 'Resources', icon: 'bi bi-bar-chart' },
+    { path: '/clients', title: 'Clients', icon: 'bi bi-people' },
+    { path: '/inventory', title: 'Inventory', icon: 'bi bi-box' }
+];
