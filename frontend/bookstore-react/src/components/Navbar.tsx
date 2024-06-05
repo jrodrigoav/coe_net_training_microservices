1e90c4a698d17c1241a237e4771e2bdc13fb837e
WAlfaroP <walfaropuerta@gmail.com>
2024-06-04 21:16:58 -0500

    Create common components

--- frontend/bookstore-react/src/components/Navbar.tsx
+++ frontend/bookstore-react/src/components/Navbar.tsx
@@ -1,8 +1,31 @@
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { routeItems } from '../routes/routeItems';
+
 const Navbar = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [title, setTitle] = useState('Dashboard');
+  const location = useLocation();
+
+  useEffect(() => {
+    const activeRoute = routeItems.find(item => location.pathname === item.path);
+    setTitle(activeRoute ? activeRoute.title : 'Dashboard');
+  }, [location]);
+
+  const toggleNavbar = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <div>
-    </div>
+    <nav className="navbar navbar-light bg-light">
+      <div className="container-fluid">
+        <button type="button" className="navbar-toggler d-md-none" onClick={toggleNavbar}>
+          <i className="bi bi-list"></i>
+        </button>
+        <span className="navbar-brand">{title}</span>
+      </div>
+    </nav>
   );
-}
+};
 
 export default Navbar;
--- frontend/bookstore-react/src/components/Sidebar.tsx
+++ frontend/bookstore-react/src/components/Sidebar.tsx
@@ -1,8 +1,43 @@
-const Sidebar = () => {
-  return (
-    <div>
-    </div>
-  );
-}
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { routeItems } from '../routes/routeItems';
+
+
+const Sidebar: React.FC = () => {
+
+    const [expanded, setExpanded] = useState(false);
+
+    const toggleSidebar = () => {
+        setExpanded(!expanded);
+      };
+
+    return (
+        <div className={`d-flex flex-column flex-shrink-0 p-3 bg-light h-100 ${expanded ? 'expanded' : ''}`}>
+            <div className="d-flex gap-2 align-items-center justify-content-md-center link-dark text-decoration-none fs-4">
+                <button type="button" className="navbar-toggler d-md-none" onClick={toggleSidebar}>
+                    <i className="bi bi-list"></i>
+                </button>
+                <i className="bi bi-book"></i>
+                <span>Bookstore</span>
+            </div>
+            <hr />
+            <ul className="nav nav-pills flex-column mb-auto">
+                {routeItems.map((item) => (
+                    <li className="nav-item" key={item.path}>
+                        <NavLink
+                            to={item.path}
+                            className={({ isActive }) =>
+                                isActive ? 'active' : ''
+                            }
+                        >
+                            <i className={item.icon}></i>
+                            {item.title}
+                        </NavLink>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default Sidebar;
