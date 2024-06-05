bf1c952c8564faabe0da3082655626667d18c38b
WAlfaroP <walfaropuerta@gmail.com>
2024-06-05 16:55:25 -0500

    Added Ant Design layout component with sider and header content

--- frontend/bookstore-react/src/components/AdminLayout.tsx
+++ frontend/bookstore-react/src/components/AdminLayout.tsx
@@ -1,18 +1,29 @@
+import React from 'react';
+import { Layout } from 'antd';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 import { Outlet } from 'react-router-dom';
 
-const AdminLayout = () => {
+const { Header, Sider, Content } = Layout;
+
+const AdminLayout: React.FC = () => {
   return (
-    <div className="main-wrapper">
-      <Sidebar />
-      <div>
-        <Navbar />
-        <div className="router-content">
-          <Outlet />
-        </div>
-      </div>
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider style={{ backgroundColor: '#e6f7ff' }}>
+        <div className="logo" />
+        <Sidebar />
+      </Sider>
+      <Layout className="site-layout" style={{ backgroundColor: '#e6f7ff' }}>
+        <Header className="site-layout-background" style={{ padding: 0, backgroundColor: '#e6f7ff' }}>
+          <Navbar />
+        </Header>
+        <Content style={{ margin: '0 16px', backgroundColor: '#e6f7ff' }}>
+          <div className="site-layout-content">
+            <Outlet />
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
   );
 };
 
